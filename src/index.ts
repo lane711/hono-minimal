77cf1d470d3691b856aba4b5f77bd366b92c0118
Lane <>
2023-05-11 09:46:18 -0700

    update hono

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-import { Hono } from 'hono'
-import { poweredBy } from 'hono/powered-by'
-import { setupApi } from './cms/api/api';
+import { Hono } from "hono";
+import { serveStatic } from "hono/cloudflare-workers";
+import { poweredBy } from "hono/powered-by";
+import { setupApi } from "./cms/api/api";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use('*', poweredBy())
+app.use("*", poweredBy());
+
+app.use("/*", serveStatic({ root: "./" }));
+app.use("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
 
 setupApi(app);
 
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-export default app
+export default app;
